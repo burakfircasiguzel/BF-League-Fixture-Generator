c0c89b2545965cc5e832080c46c348b09d10f882
Burak Fircasiguzel <burakfguzel@gmail.com>
2023-01-05 20:22:22 +0300

    add image

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Card, CardBody, CardHeader, CardText, CardTitle, Nav, NavItem, NavLink } from 'reactstrap'
-
+import logo512 from './assets/logo512.png';
+import logo from './assets/logo192.png';
 const Navbar = () => {
     return (
 
         <Card className='mt-2'>
             <CardBody>
                 <CardText>
-                    <b>BF Fixture | Fixture.website 🚀</b>
+                <img src={logo} style={{width : '20px', alignItems : 'center' }} className='ml-2'></img>
+                    <b>  BF Fixture | Fixture.website 🚀</b>
+                    
                 </CardText>
             </CardBody>
         </Card>
